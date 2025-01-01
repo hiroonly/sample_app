c1cdc2b126125522219bdcf9351f34137b038a25
hiro <tobi.pc@outlook.jp>
2025-01-01 20:44:36 +0000

    finish ch3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # GET / => static_pages#home
+  root 'static_pages#index'
+
+  # GET / static_pages/home => static_pages#home
   get  'static_pages/home'
   get  'static_pages/help'
   get  'static_pages/about'
-  root 'hello#index'
 end
