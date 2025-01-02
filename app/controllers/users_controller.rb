760090af7d59b1720f24ea7d1a7e5523c9475ec9
hiro <tobi.pc@outlook.jp>
2025-01-02 23:11:49 +0000

    ch7 owari

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
-  def new
+  # GET /users/:id
+  def show
+    #@user = User.first
+    @user = User.find(params[:id])
   end
+
+  # GET /users/new
+  def new
+    @user = User.new
+    # => app/views/users/new.html.erb
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # => Success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      # =>Failure
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Strong Parameter
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
